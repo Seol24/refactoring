a20863adf52d9724c4ddc560c05b83aaa47c6057
seol24 <ehdgnsdms321@gmail.com>
2017-03-20 11:24:16 +0900

    리펙토링 커밋

--- src/refactoring/dto/Movie.java
+++ src/refactoring/dto/Movie.java
@@ -1,9 +1,9 @@
 package refactoring.dto;
 
 public class Movie {
-/*	public static final int REGULAR = 0;
-	public static final int NEW_RELEASE = 1;
-	public static final int CHILDRENS = 2;*/
+	public static final int REGULAR = 1;
+	public static final int NEW_RELEASE = 2;
+	public static final int CHILDRENS = 3;
 	
 	
 	private String title;
