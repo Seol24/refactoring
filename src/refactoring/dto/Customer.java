c683fa33f76b2fb762dc38ac8a015a548316e998
seol24 <ehdgnsdms321@gmail.com>
2017-03-20 11:47:16 +0900

    고객 요구사항 추가 ....(html결과물, 대여료, 적립포인트 수정) 기능적 재사용 불가 (ctrl+c, ctrl+v)

--- src/refactoring/dto/Customer.java
+++ src/refactoring/dto/Customer.java
@@ -28,25 +28,27 @@ public class Customer {
       
       for(Rental each : rentals){
          double thisAmount = 0;
-         // 비디오물당 대여료 1. 일반물 (2일) 2000원, 일일초과 1500원 , 적립1
-                  //2. 아동물 (3일) 1500원, 일일초과 1500원, 적립1
-                  //3. 최신물 (1일) 3000원, 일일초과 3000원, 적립1
+         // 비디오물당 대여료
+         //1. 일반물 (2일) 2000원, 일일초과 1500원, 적립1 ==>(2일, 3000원) 일일 초과 2000, 적립1
+         //2. 아동물 (3일) 1500원, 일일초과 1500원, 적립1 ==>(2일, 2000원) 일일 초과 1500, 적립1
+         //3. 최신물 (1일) 3000원, 일일초과 3000원, 적립1 + 1 ==>(2일, 4000원) 일일 초과 4000, 적립 1+1
+         //HTml형식을 출력 원함.
          Movie movie = each.getMovie();
          
          int pirceCode = movie.getPriceCode();
          int daysRented = each.getDaysRented(); //해당 비디오물의 대여기간
          switch(pirceCode){
          case Movie.REGULAR:
-            thisAmount = 2000;
+            thisAmount = 3000;
             if (daysRented > 2) {
-               thisAmount += (daysRented - 2) * 1500;
+               thisAmount += (daysRented - 2) * 2000;
             }
             break;
          case Movie.NEW_RELEASE:
-            thisAmount = (daysRented * 3000);
+            thisAmount = (daysRented * 4000);
             break;
          case Movie.CHILDRENS:
-            thisAmount = 1500;
+            thisAmount = 3000;
             if (daysRented > 3) {
                thisAmount += (daysRented - 3) * 1500;
             }
@@ -64,4 +66,52 @@ public class Customer {
       sb.append(String.format("누적대여료: %s%n적립포인트 : %s%n", totalAmount, frequentRenterPoints));
       return sb.toString();
    }
+   public String htmlstatement(){
+	      double totalAmount = 0;//총 대여료
+	      int frequentRenterPoints = 0;//적립포인트
+	      StringBuilder sb = new StringBuilder();
+	      sb.append("<h1><Em>getName()+"+getName()+"고객님의 대여기록\n</Em></h1><br>");
+	      
+	      for(Rental each : rentals){
+	         double thisAmount = 0;
+	         // 비디오물당 대여료
+	         //1. 일반물 (2일) 2000원, 일일초과 1500원, 적립1 ==>(2일, 3000원) 일일 초과 2000, 적립1
+	         //2. 아동물 (3일) 1500원, 일일초과 1500원, 적립1 ==>(2일, 2000원) 일일 초과 1500, 적립1
+	         //3. 최신물 (1일) 3000원, 일일초과 3000원, 적립1 + 1 ==>(2일, 4000원) 일일 초과 4000, 적립 1+1
+	         //HTml형식을 출력 원함.
+	         Movie movie = each.getMovie();
+	         
+	         int pirceCode = movie.getPriceCode();
+	         int daysRented = each.getDaysRented(); //해당 비디오물의 대여기간
+	         switch(pirceCode){
+	         case Movie.REGULAR:
+	            thisAmount = 3000;
+	            if (daysRented > 2) {
+	               thisAmount += (daysRented - 2) * 2000;
+	            }
+	            break;
+	         case Movie.NEW_RELEASE:
+	            thisAmount = (daysRented * 4000);
+	            break;
+	         case Movie.CHILDRENS:
+	            thisAmount = 3000;
+	            if (daysRented > 3) {
+	               thisAmount += (daysRented - 3) * 1500;
+	            }
+	            break;
+	         }
+	         frequentRenterPoints++;
+	         if(each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() >1){
+	            frequentRenterPoints++;
+	         }
+	         sb.append(String.format("nbsp;nbsp;%s nbsp;nbsp; %s<br>", each.getMovie().getTitle(), thisAmount));
+	         
+	         totalAmount += thisAmount;
+	         //for loop
+	      }
+	    sb.append(String.format("누적대여료: %s%n적립포인트 : %s%n", totalAmount, frequentRenterPoints));  
+		return sb.toString();
+   
+   
+}
 }
