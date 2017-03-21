52eef456be77a8bdad554dbb4a19bda8e12db6fc
seol24 <ehdgnsdms321@gmail.com>
2017-03-21 10:40:24 +0900

    임시변수를 메서드 호출로 전환(getTotalCharge(), getTotal)

--- src/refactoring/dto/Customer.java
+++ src/refactoring/dto/Customer.java
@@ -21,21 +21,33 @@ public class Customer {
 	}
 
 	public String statement() {
-		double totalAmount = 0;// 대여료
-		int frequentRenterPoints = 0;// 적립포인트
+		//대여료와 적립포인트 출력
 		StringBuilder sb = new StringBuilder();
 		sb.append(getName() + "고객님의 대여기록\n");
 
 		for (Rental each : rentals) {
-			frequentRenterPoints += each.getFrequentRentalPoints();
-			
 			sb.append(String.format("\t%s \t %s%n", each.getMovie().getTitle(), each.getCharge()));
-
-			totalAmount += each.getCharge();
 		}// for loop
 		
-		sb.append(String.format("누적대여료: %s%n적립포인트 : %s%n", totalAmount, frequentRenterPoints));
+		sb.append(String.format("누적대여료: %s%n적립포인트 : %s%n", getTotalCharge(), gettTotalFrequentRenterPoints()));
+		
 		return sb.toString();
+	}
+
+	private int gettTotalFrequentRenterPoints() {
+		int result = 0;
+		for (Rental each : rentals) {
+			result += each.getFrequentRentalPoints();
+		}
+		return result;
+	}
+
+	private double getTotalCharge() {
+		double result = 0;
+		for (Rental each : rentals) {
+			result += each.getCharge();
+		}
+		return result;
 	}
 	
 	
